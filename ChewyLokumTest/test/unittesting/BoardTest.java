93dd293f81a4f57f2fe5c1be722f8018594a41a6
yigitkumral <ykumral@ku.edu.tr>
2014-12-25 23:08:14 +0200

    Update
    Update

--- ChewyLokumTest/test/unittesting/BoardTest.java
+++ ChewyLokumTest/test/unittesting/BoardTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import game.*;
 public class BoardTest {
-	//deneme
+	//taylan
 	@Before
 	public void setUp() throws Exception {
 		Board testBoard = new Board(6,6);
