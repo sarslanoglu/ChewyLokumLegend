3c9b5c032594423f8774356ed4cfcf4cb79d4e90
yigitkumral <ykumral@ku.edu.tr>
2014-12-26 11:35:28 +0200

    Javadoc update

--- ChewyLokumTest/src/game/Combination.java
+++ ChewyLokumTest/src/game/Combination.java
@@ -19,7 +19,7 @@ public class Combination {
 	/**
 	 * 
 	 * @param type      the type of the combination
-	 * @param Lokums    the list of the lokums that uses combination.
+	 * @param Lokums    the list of the lokums that used in combination.
 	 */
 
 
@@ -45,7 +45,7 @@ public class Combination {
 
 	/**
 	 * 
-	 * @return checks each lokum and returns true if lokum is special.
+	 * @return checks size of lokums array list and returns true if size is exactly 2.
 	 */
 	public boolean isSpecial() {
 		if(Lokums.size() == 2){
--- ChewyLokumTest/src/game/SavedGame.java
+++ ChewyLokumTest/src/game/SavedGame.java
@@ -2,25 +2,24 @@ package game;
 /**
  * 
  * SavedGame is a class
- * that has a name, board, score and swapsLeft.
+ * that has a board, score and swapsLeft.
  *
  */
 public class SavedGame {
 	
 	/**
 	 * 
-	 * @param name		 the name that player chooses. 
 	 * @param board		 saved board.
 	 * @param score		 the score of the player in the game.
 	 * @param swapsLeft  the amount of swaps of the player.
 	 */
 	
-	public SavedGame(String name,Board board,int score,int swapsLeft,int Level, long time){
+	public SavedGame(Board board,int score,int swapsLeft,int Level, long time){
 		
 		/**
 		 * Default constructor of savedGame.
 		 * Constructs a saved game object with
-		 * a saved name, a saved board, a saved score
+		 * a saved board, a saved score
 		 * and the amount of left swaps of the player.
 		 */
 		
