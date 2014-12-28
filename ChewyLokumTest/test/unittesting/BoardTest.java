91184b4f9dbee3ecfbaa2c7f2f89d777c91e0b4b
sarslanoglu <sarslanoglu@ku.edu.tr>
2014-12-28 12:14:08 +0200

    Glass Box Additions

--- ChewyLokumTest/test/unittesting/BoardTest.java
+++ ChewyLokumTest/test/unittesting/BoardTest.java
@@ -91,15 +91,15 @@ public class BoardTest {
 		normalLokum lok2 = new normalLokum(1,1,"B",false);
 		assertTrue(lok1.repOk());
 		assertTrue(lok2.repOk());
-		
+
 		testBoard.set(lok1.getPositionX(),lok1.getPositionY(),lok1);
 		testBoard.set(lok2.getPositionX(),lok2.getPositionY(),lok2);
 		testBoard.unswap(lok1,lok2);
-		
+
 		assertTrue(lok1.repOk());
 		assertTrue(lok2.repOk());
 		assertTrue(testBoard.repOk());
-		
+
 		assertTrue(lok1.getPositionX()==1 && lok1.getPositionY() == 1
 				&& lok2.getPositionX()==0 && lok2.getPositionY() == 0
 				&& testBoard.get(1,1)==lok1 && testBoard.get(0,0) == lok2);
@@ -175,16 +175,16 @@ public class BoardTest {
 		testBoard = new Board(5,5);
 		normalLokum a = new normalLokum (0,0,"W",false);
 		normalLokum b = new normalLokum (1,1,"G",false);
-		
+
 		testBoard.set(a.getPositionX(),a.getPositionY(),a);
 		testBoard.set(b.getPositionX(),b.getPositionY(),b);
-		
+
 		int tempx = b.getPositionX();
 		int tempy = b.getPositionY();
 		testBoard.unswap(a, b);
 		int tempx1 = a.getPositionX();
 		int tempy1 = a.getPositionY();
-		
+
 		if(tempx == tempx1 || tempy == tempy1){
 			assertTrue(a.repOk());
 			assertTrue(b.repOk());
@@ -192,7 +192,7 @@ public class BoardTest {
 			assertFalse(a.repOk());
 			assertFalse(b.repOk());
 		}
-		
+
 	}
 	@Test
 	public void specialSwapActionTest(){
@@ -202,7 +202,80 @@ public class BoardTest {
 
 		testBoard.set(c.getPositionX(),c.getPositionY(),c);
 		testBoard.set(d.getPositionX(),d.getPositionY(),d);
+
+		// Special swap olup olmadigini check etmeli ama nasil
+		// testBoard.swap(c, d);
+
+		int tempx = d.getPositionX();
+		int tempy = d.getPositionY();
+		testBoard.swap(c, d);
+		int tempx1 = c.getPositionX();
+		int tempy1 = c.getPositionY();
+
+		if(tempx == tempx1 || tempy == tempy1){
+			assertTrue(c.repOk());
+			assertTrue(d.repOk());
+		}else{
+			assertFalse(c.repOk());
+			assertFalse(d.repOk());
+		}
 	}
+
+	@Test
+	public void scoreTest(){
+		testBoard = new Board(5,5);
+		testBoard.constructRandomBoard();
+
+		testBoard.setScore(0);
+		normalLokum lokum1 = new normalLokum (0,0,"W",false);
+		normalLokum lokum2 = new normalLokum (0,1,"W",false);
+		normalLokum lokum3 = new normalLokum (0,2,"W",false);
+		testBoard.set(0, 0, lokum1);
+		testBoard.set(0, 1, lokum2);
+		testBoard.set(0, 2, lokum3);
+
+		ArrayList<Combination> c = testBoard.checkCombinations();
+
+		// Array list in combination la uyusmazligi sorunu var sanýrým
+
+		//		testBoard.calculateCombinationScore(c);
+		//		testBoard.eat(c);
+
+		if(testBoard.getScore() != 0){
+			assertTrue(testBoard.repOk());
+		}else{
+			assertFalse(testBoard.repOk());
+		}
+	}
+
+	@Test
+	public void timeCombinationTest(){
+		testBoard = new Board(5,5);
+		testBoard.constructRandomBoard();
+
+		testBoard.setScore(0);
+		testBoard.setTime(10000);
+		normalLokum lokum1 = new normalLokum (0,0,"W",true);
+		normalLokum lokum2 = new normalLokum (0,1,"W",true);
+		normalLokum lokum3 = new normalLokum (0,2,"W",true);
+		testBoard.set(0, 0, lokum1);
+		testBoard.set(0, 1, lokum2);
+		testBoard.set(0, 2, lokum3);
+
+		ArrayList<Combination> c = testBoard.checkCombinations();
+
+		// Array list in combination la uyusmazligi sorunu var sanýrým
+
+		//		testBoard.calculateCombinationScore(c);
+		//		testBoard.eat(c);
+
+		if(testBoard.getTime() > 10000){
+			assertTrue(testBoard.repOk());
+		}else{
+			assertFalse(testBoard.repOk());
+		}
+	}
+
 }
 
 
